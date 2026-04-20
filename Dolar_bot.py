188aee3cd085585eafe974edabf126462cd2e202
edisoncardozo900-web <edisoncardozo900@gmail.com>
2026-04-20 12:13:21 -0400

    Initialize Dolar_bot repository with README and git setup

---
+++ Dolar_bot.py
@@ -0,0 +1,7 @@
+echo "# Dolar-bot" >> README.md 
+git init 
+git add README.md 
+git commit -m "primer commit" 
+git branch -M main 
+git remote add origin https://github.com/edisoncardozo900-web/Dolar-bot.git
+ git push -u origin main
